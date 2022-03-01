44999bc6ead6a7fc929572b61e30f1e70a838255
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-03-01 17:02:04 +0100

    adding opacity to the bookmark

--- js/script.js
+++ js/script.js
@@ -31,28 +31,50 @@ for (let i = 0; i < items.length; i++) {
   </div>`
 }
 // I take the wrapper of the img
-let wrapper=document.querySelector(".col-10");
+let wrapper=document.querySelector(".col-8");
 // I put the images in the wrapper
 wrapper.innerHTML += carouselContent;
-// i take the button
-const btnUp=document.querySelector(".up");
-const btnDown=document.querySelector(".down");
 // i take the img container
-const carouselItems=document.getElementsByClassName("my-cards")
+const carouselItems=document.getElementsByClassName("my-cards");
 // I remove the class d-none from the first image
 carouselItems[0].classList.remove("d-none");
 console.log(carouselItems);
+// I create the bookmark variable
+let bookmark="";
+// loop to insert images into array
+for(let i=0; i < items.length; i++){
+  bookmark+=`
+    <div class="container-card opacity">
+      <img src="${items[i]}"  alt="${title[i]} img"> 
+    </div>  
+  `
+}
+// I take the wrapper of the img bookmark
+let wrapperBookmark=document.querySelector(".container-bookmark");
+wrapperBookmark.innerHTML+=bookmark;
+// i take the container of single item
+const bookmarkItem=document.getElementsByClassName("container-card");
+// I remove the class opacity from the first image
+bookmarkItem[0].classList.remove("opacity")
+// i take the button
+const btnUp=document.querySelector(".up");
+const btnDown=document.querySelector(".down");
 
 let indexImg=0;
 btnDown.addEventListener('click', function () {
   carouselItems[indexImg].classList.add("d-none");
+  bookmarkItem[indexImg].classList.add('opacity');
   indexImg++;
   carouselItems[indexImg].classList.remove("d-none");
+  bookmarkItem[indexImg].classList.remove('opacity');
+
 
 })
 btnUp.addEventListener('click', function () {
+  bookmarkItem[indexImg].classList.add('opacity');
   carouselItems[indexImg].classList.add("d-none");
   indexImg--;
+  bookmarkItem[indexImg].classList.remove('opacity');
   carouselItems[indexImg].classList.remove("d-none");
 
 })
